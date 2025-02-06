1ed3adaa4b8c8fcf35154b28e639cdf38d057c48
Stahl_PC <dannyrdc@sas.upenn.edu>
2025-02-06 17:16:29 -0600

    save and switch

--- reaxysProcessing/ExampleDatabase/reaxysCompFeatureMaping.py
+++ reaxysProcessing/ExampleDatabase/reaxysCompFeatureMaping.py
@@ -23,7 +23,7 @@ def createCSV(dataFrame , saveDir, saveStr: str):
     if not os.path.exists(saveDir):
         os.makedirs(saveDir)
 
-    dataFrame.to_csv(saveDir + "/" + saveStr + ".xlsx", index=False) 
+    dataFrame.to_csv(saveDir + "/" + saveStr + ".csv", index=True) 
     return dataFrame
 
 def DimensionalityReduction(X, Y , labels, partition1 ):
@@ -34,10 +34,25 @@ def DimensionalityReduction(X, Y , labels, partition1 ):
 
 if __name__ == "__main__":
 #input an xlsx dataframe and depending on what features you want to calculate, export .csv of features, performs feature elimination, maps features, and creates clusters 
-mainDir = str(sys.argv[1])
-rdkitAllow = int(sys.argv[2])
-moldrAllow = int(sys.argv[3])
-maccAllow = int(sys.argv[4])
+    mainDir = str(sys.argv[1])
+    rdkitAllow = int(sys.argv[2])
+    moldrAllow = int(sys.argv[3])
+    maccAllow = int(sys.argv[4])
+    
+    featureTypes = []
+    if rdkitAllow == 1:
+        rdkitSave = "rdkit"
+        featureTypes.append(rdkitSave)
+    if moldrAllow == 1:
+        moldrSave = "moldred"
+        featureTypes.append(moldrSave)
+    if maccAllow == 1:
+        maccSave = "MACCSkeys"
+        featureTypes.append(maccSave)
+    #defines options for features you want 
+
+    
+    
 
 
     
