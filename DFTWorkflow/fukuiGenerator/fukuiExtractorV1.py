a315df52ee4831ab0efddcff79ad6f4559548dc7
danny-ruizdecastilla <dannyrdc@sas.upenn.edu>
2025-05-20 15:02:30 -0500

    update

--- DFTWorkflow/fukuiGenerator/fukuiExtractorV1.py
+++ DFTWorkflow/fukuiGenerator/fukuiExtractorV1.py
@@ -11,8 +11,7 @@ from DFTWorkflow.ionComGenerator import locateinLog
 
 #Danny Ruiz de Castilla
 #Generates Fukui maps for each molecule 
-def fukuiMap(substrate:dict):
-    substrateDF = pd.DataFrame()
+def fukuiFunction(substrateDF):
     for atom , presences in enumerate(substrate):
         #presences = [neutral , cation , anion]
         f_0 = 0.5*(float(presences[2]) - float(presences[1]) )
@@ -56,7 +55,40 @@ def main(logDir , cationDir, anionDir ):
     for log in neutralLogs:
         logList = []
         logName = log.split("/")[-1].split(".")[0]
-        logList.append(logName)
+        logList.append(log)
+        if os.path.exists(cationDir + "/" + logName + "_cation.log"):
+            logList.append(cationDir + "/" + logName + "_cation.log")
+        else:
+            print("Missing the cation .log File for " + logName)
+            continue
+        if os.path.exists(anionDir + "/" + logName + "_anion.log"):
+            logList.append(anionDir + "/" + logName + "_anion.log")     
+         
+        else:
+            print("Missing the anion .log File for " + logName)
+            continue
+        if len(logList) == 3:
+            substrateDicts[logName] = logList
+    substrateDictMAST = {}
+    for substrate, dirs in substrateDicts.items():
+        substrateDF = pd.DataFrame()
+        for dir in dirs:
+            name = dir.split("/")[-1]
+            #print(name)
+            if "ion" in name:
+                name1 = name.split("_")[-1].split(".")[0]
+            else:
+                name1 = "neutral"
+            occupanciesDict = extractOccupancies(dir,"Core      Valence    Rydberg" , "* Total *" , "earliest")
+            if not "atoms" in substrateDF.columns:
+                substrateDF["atoms"] = occupanciesDict.keys()
+            substrateDF[name1 + "_occupancies"] = list(occupanciesDict.values())
+
+        
+        substrateDictMAST[substrate] = substrateDF
+
+
+
         
 
 if __name__ == "__main__":
