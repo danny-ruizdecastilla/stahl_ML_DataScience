176b173ed1432fdaf719233d78ee404ec0d8d8ee
Stahl_PC <dannyrdc@sas.upenn.edu>
2025-06-02 12:11:48 -0500

    update

--- breadthFirstSearch/fukuiExtractor_Alkenes.py
+++ breadthFirstSearch/fukuiExtractor_Alkenes.py
@@ -13,6 +13,22 @@ sys.path.append(parentDir)
 from DFTWorkflow.fukuiGenerator.addSCF import readDat
 from DFTWorkflow.pitchingATent import convertCanonical 
 from DFTWorkflow.featureMaping import createCSV
+def dict2Dat(dict1, saveDir, saveStr):
+    file_path = os.path.join(saveDir, saveStr + ".dat")
+    
+    if not os.path.exists(file_path):
+        with open(file_path, "w") as file:
+            for key, value in dict1.items():
+                value_str = ",".join(str(a) for a in value)
+                file.write(f"{key}:{value_str}\n")
+def getMaxCoordinate( allPaths , coordinate , initList):
+    coordinateList = []
+    for path in allPaths:
+        newPath = [atom for atom in path if atom not in initList]
+        coordinateList.append(int(newPath[coordinate]))
+    coordinateList = list(set(coordinateList))
+    return coordinateList
+
 def getC1C2(smiles, cutDist):
     ptable = Chem.GetPeriodicTable()
     contactsDict , smilesEdgesDict , allPaths , g, molec = motifExtract(smiles, cutDist)
@@ -31,50 +47,69 @@ def getC1C2(smiles, cutDist):
     else:
         C1 = atoms[1]
         C2 = atoms[0]
-    return C1 , C2
+    wildList = getMaxCoordinate( allPaths,0,[C1 , C2]  )
+    return C1 , C2 , wildList
 
-def main(substratesDir:str, contactDist: int  ):
+def main(substratesDir:str, contactDist: int  , saveDir):
 
     substratePaths = glob.glob(substratesDir + "/*/identification.dat")
-    substrateDF = pd.DataFrame(columns = ["SMILES" , "Canonicals" ,"C1_fuk_neg" , "C1_fuk_pos" ,
-                                           "C1_fuk_neut" ,"C2_fuk_neg" , "C2_fuk_pos" , "C2_fuk_neut" 
-                                             ,"delta_fuk_neg" , "delta_fuk_pos" , "delta_fuk_neut"  ])
+    substrateDF = pd.DataFrame(columns = ["SMILES" , "Canonicals" ,"C1_fuk_neg" , "C1_fuk_pos" ,"C1_fuk_neut" ,
+                                          "C2_fuk_neg" , "C2_fuk_pos" , "C2_fuk_neut" 
+                                             ,"delta_fuk_neg" , "delta_fuk_pos" , "delta_fuk_neut"  , 
+                                             "CMax_fuk_neg" , "CMax_fuk_pos" , "CMax_fuk_neut",
+                                             "maxWild_fuk_neg", "maxWild_fuk_pos" , "maxWild_fuk_neut"  ])
     for i , path in enumerate(substratePaths):
         identifications = readDat(path)
         smiles = identifications[-1]
 
-        C1 , C2 = getC1C2(smiles , contactDist)
+        C1 , C2 , wildList = getC1C2(smiles , contactDist)
         C1 += 1 #switch to log file indexing 
         C2 += 1
+        wildList += 1 
+        #print(identifications[0])
+        #print("C1" , C1 , "C2" , C2)
+        wildDict = {}
         mainPath = path.split("identification.dat")[0]
         fukuiData = glob.glob(mainPath + "/*.csv")[0]
         fukuiPD = pd.read_csv(fukuiData)
         C1List = C2List = None
         for _, row in fukuiPD.iterrows():
             atom = int(row["atoms"])
+            #print(atom)
             f_neg, f_pos, f_neut = row["f_neg"], row["f_pos"], row["f_neut"]
 
             if atom == C1:
                 C1List = [f_neg, f_pos, f_neut]
             elif atom == C2:
                 C2List = [f_neg, f_pos, f_neut]
+            elif atom in wildList:
+                wildDict[atom] = [f_neg, f_pos, f_neut]
         if C1List is None or C2List is None:
             raise ValueError("C1 or C2 not found in Fukui data")
-
+        dict2Dat(wildDict, saveDir , "WildFukuiVals")
+        wild0 = []
+        wildPlus = []
+        wildNeg = []
+        for wild , fukuiList in wildDict.items():
+            wild0.append(fukuiList[2])
+            wildPlus.append(fukuiList[1])
+            wildNeg.append(fukuiList[0])
+        maxWild_0 = max(wild0)
+        maxWild_neg = max(wildNeg)
+        maxWild_pos = max(wildPlus)
+        maxFukNeg = max([C1List[0] , C2List[0]])
+        maxFukPos = max([C1List[1] , C2List[1]])
+        maxFukNeut = max([C1List[2] , C2List[2]])
         deltaFuk = [abs(a - b) for a, b in zip(C1List, C2List)]
         canonical = convertCanonical(smiles)
         newRow = pd.DataFrame([{
             "SMILES": smiles,
             "Canonicals": canonical,
-            "C1_fuk_neg": C1List[0],
-            "C1_fuk_pos": C1List[1],
-            "C1_fuk_neut": C1List[2],
-            "C2_fuk_neg": C2List[0],
-            "C2_fuk_pos": C2List[1],
-            "C2_fuk_neut": C2List[2],
-            "delta_fuk_neg": deltaFuk[0],
-            "delta_fuk_pos": deltaFuk[1],
-            "delta_fuk_neut": deltaFuk[2]
+            "C1_fuk_neg": C1List[0],"C1_fuk_pos": C1List[1],"C1_fuk_neut": C1List[2],
+            "C2_fuk_neg": C2List[0],"C2_fuk_pos": C2List[1],"C2_fuk_neut": C2List[2],
+            "delta_fuk_neg": deltaFuk[0],"delta_fuk_pos": deltaFuk[1],"delta_fuk_neut": deltaFuk[2], 
+            "CMax_fuk_neg": maxFukNeg , "CMax_fuk_pos":maxFukPos , "CMax_fuk_neut": maxFukNeut,
+            "maxWild_fuk_neg" : maxWild_neg, "maxWild_fuk_pos": maxWild_pos , "maxWild_fuk_neut": maxWild_0  
         }])
         substrateDF = pd.concat([substrateDF, newRow], ignore_index=True)
     return substrateDF
@@ -83,5 +118,5 @@ if __name__ == "__main__":
     contactDist = int(sys.argv[2])
     saveDir = str(sys.argv[3])
     saveStr = str(sys.argv[4])
-    substrateDF = main(fukuiOutputs , contactDist)
+    substrateDF = main(fukuiOutputs , contactDist , saveDir)
     createCSV(substrateDF , saveDir , saveStr)
