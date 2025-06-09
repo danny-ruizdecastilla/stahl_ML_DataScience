cf26eead0d420a767ad413f76f3703ae827c2572
danny-ruizdecastilla <dannyrdc@sas.upenn.edu>
2025-06-08 23:50:57 -0400

    update

--- DFTWorkflow/ionComGenerator.py
+++ DFTWorkflow/ionComGenerator.py
@@ -1,4 +1,4 @@
-import os 
+import os
 import sys
 import glob
 import shutil
@@ -21,9 +21,15 @@ def theoryLevel(option):
     if option == 1:
         functional = "M062X"
         basisSet = "def2tzvp"
-    if option == 2:
+    elif option == 2:
         functional = "B3LYP"
         basisSet = "6-31G(d,p)"
+    elif option == 3:
+        functional = "B3LYP"
+        basisSet = "6-311++G(d,p)"
+    elif option ==4:
+        functional = "B3LYP"
+        basisSet = "def2tzvp"
     return functional + "/" + basisSet
 def comWriter(comFile:str, **kwargs ):
     if kwargs.get("coordinates") is None:
@@ -35,8 +41,9 @@ def comWriter(comFile:str, **kwargs ):
         mem = int(kwargs["mem"])
         chk = str(kwargs["chk"])
         theory = str(kwargs["theory"])
-        netCharge = int(kwargs["electron"]) 
+        netCharge = int(kwargs["electron"])
         spin = int(kwargs["spin"])
+        iop = str(kwargs["iop"])
     except KeyError as e:
         raise ValueError(f"Missing required parameter: {e}")
     if kwargs.get("geom") is None:
@@ -66,14 +73,14 @@ def comWriter(comFile:str, **kwargs ):
         f.write(f"%chk={chk}\n")
         fileStr = chk.split("/")[-1].split(".")[0]
         if fromChk:
-            f.write(f"#{theory} geom={geom} pop={pop} guess={guess} symmetry={symmetry} "
+            f.write(f"#{theory} geom={geom} pop={pop}{iop}guess={guess} symmetry={symmetry} "
                     "int=(grid=ultrafine) SP\n\n")
             f.write("Commentline\n\n")
             f.write(f"{netCharge} {spin}\n\n")
 
         else:
-            f.write(f"#{theory} symmetry={symmetry} pop={pop} "
-                    "scf=qc int=(grid=ultrafine) SP\n\n") 
+            f.write(f"#{theory} symmetry={symmetry} pop={pop}{iop}"
+                    "scf=qc int=(grid=ultrafine) SP\n\n")
             f.write(f"{fileStr}.xyz\n\n")
             f.write(f"{netCharge} {spin}\n")
             coordDict = kwargs["coordinates"]
@@ -81,11 +88,11 @@ def comWriter(comFile:str, **kwargs ):
                 row = coordDict[atom]
                 f.write(f"{','.join(str(i) for i in row)}\n")
             f.write("\n\n")
-        
+
     return fileStr
 def locateinLog(logFile, textStr, returnType: str):
     matchingLines = []
-    
+
     with open(logFile, "r") as f:
         for idx, line in enumerate(f):
             if textStr in line:
@@ -101,7 +108,7 @@ def locateinLog(logFile, textStr, returnType: str):
         print("Bad Log File")
         return "Poison"
 def getAtomCoords(logFile , xyzStr , commaSplit:int , ):
-    #Extracts atom coordinates into a dict from a log file 
+    #Extracts atom coordinates into a dict from a log file
     atomCoords = {}
     lowerIdx = locateinLog(logFile , xyzStr, "latest" )
     upperIdx = locateinLog(logFile, "The archive entry for this job was punched." , "latest")
@@ -121,7 +128,7 @@ def getAtomCoords(logFile , xyzStr , commaSplit:int , ):
         return atomCoords
     else:
         return "Poison"
-def main(logDir , netCharge , comDir , chkDir , popType, theory):
+def main(logDir , netCharge , comDir , chkDir , popType, theory, iop):
     if netCharge == 0:
         ion = "neutral"
         spin_ = 1
@@ -135,7 +142,7 @@ def main(logDir , netCharge , comDir , chkDir , popType, theory):
     logs = glob.glob(logDir + "/*.log")
     print(logs)
     ionComDir = comDir + "/" + ion + "s"
-    if not os.path.exists(ionComDir): 
+    if not os.path.exists(ionComDir):
         os.makedirs(ionComDir)
     for log in logs:
         file = log.split("/")[-1]
@@ -151,12 +158,12 @@ def main(logDir , netCharge , comDir , chkDir , popType, theory):
                 continue
             else:
                 fileName = comWriter(ionComDir + "/" + fileName , nprocs = int(16) , mem = int(48) , theory = str(theory) , chk =chkFile ,
-                             electron =  netCharge , spin = spin_ , coordinates = atomsDict , pop = popType )          
+                             electron =  netCharge , spin = spin_ , coordinates = atomsDict , pop = popType, iop = iop )
         else:
             chkFile = copyChks(chkFile , ionComDir)
 
             fileName = comWriter(ionComDir + "/" + fileName , nprocs = int(16) , mem = int(48) , theory = str(theory) , chk =chkFile ,
-                              geom = "checkpoint" , electron =  netCharge , spin = spin_ , pop = popType )
+                              geom = "checkpoint" , electron =  netCharge , spin = spin_ , pop = popType, iop =iop )
 
 
 if __name__ == "__main__":
@@ -165,5 +172,9 @@ if __name__ == "__main__":
     comDir = str(sys.argv[3])
     chkDir = str(sys.argv[4])
     popType = str(sys.argv[5])
+    if popType == "chelpg":
+        iop = " iop(6/50=1) "
+    else:
+        iop = " "
     theory = int(sys.argv[6])
-    main(logDir , netCharge , comDir , chkDir , popType, theory)
+    main(logDir , netCharge , comDir , chkDir , popType, theory, iop)
