de30565dc08a7c326ceb540c9aefbcd8a4eebef5
Stahl_PC <dannyrdc@sas.upenn.edu>
2025-09-23 17:13:58 -0500

    update

--- DFTWorkflow/conformerSearching/smiles_to_xyz.py
+++ DFTWorkflow/conformerSearching/smiles_to_xyz.py
@@ -7,6 +7,7 @@ import pandas as pd
 parentDir = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
 sys.path.append(parentDir)
 from DFTWorkflow.AlkeneFeatures.alkeneSubstitution import  smiles_to_coords
+from dimensionalityReduction.reactivityFeatures import boxGen
 def write_xyz(elements, coords, xyzPath):
     numAtoms = len(elements)
     with open(xyzPath, "w") as f:
@@ -16,9 +17,27 @@ def write_xyz(elements, coords, xyzPath):
 
 def main(csvDir , outputDir):
     df = pd.read_csv(csvDir)
+    colList = list(df.columns)
+    colBox = boxGen(colList)
+    while True:
+        smileIdx = input(f"Here are the columns provided in the dataframe: {csvDir} Enter the number for the column corresponding to the SMILES strings {colBox}")
+        try:
+            smilesIdx = int(smileIdx)
+            smilesCol = colList[smilesIdx]
+            break
+        except:
+            print("Error: please enter an appropriate integer")
+    while True:
+        idIdx = input(f"Here are the columns provided in the dataframe: {csvDir} Enter the number for the column corresponding to the ID strings {colBox}")
+        try:
+            idIdx = int(idIdx)
+            idCol = colList[idIdx]
+            break
+        except:
+            print("Error: please enter an appropriate integer")
     for _, row in df.iterrows():
-        smiles = row["canonicals"]
-        id = row["ID"]
+        smiles = row[smilesCol]
+        id = row[idCol]
         elements , coords = smiles_to_coords(smiles)
         xyzDir = outputDir + "/" + str(id)
         xyzFile = xyzDir + "/" + str(id) + ".xyz"
