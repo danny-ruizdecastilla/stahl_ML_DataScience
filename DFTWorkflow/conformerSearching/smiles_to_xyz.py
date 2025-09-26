49022f576d465ee10d448920308031219c16316e
Stahl_PC <dannyrdc@sas.upenn.edu>
2025-09-26 08:57:44 -0500

    update

--- DFTWorkflow/conformerSearching/smiles_to_xyz.py
+++ DFTWorkflow/conformerSearching/smiles_to_xyz.py
@@ -1,54 +1,70 @@
-#Danny Ruiz de Castilla 06.22.2025
+# Danny Ruiz de Castilla 06.22.2025
 import sys
 import glob
 import numpy as np
 import os
 import pandas as pd
-parentDir = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
-sys.path.append(parentDir)
-from DFTWorkflow.AlkeneFeatures.alkeneSubstitution import  smiles_to_coords
+from pathlib import Path
+
+parentDir = Path(__file__).resolve().parents[2]
+sys.path.append(str(parentDir))
+
+from DFTWorkflow.AlkeneFeatures.alkeneSubstitution import smiles_to_coords
 from dimensionalityReduction.reactivityFeatures import boxGen
+
+
 def write_xyz(elements, coords, xyzPath):
     numAtoms = len(elements)
     with open(xyzPath, "w") as f:
         f.write(f"{numAtoms}\n\n")
-        for i in range (len(elements)):
+        for i in range(len(elements)):
             f.write(f"{elements[i]}  {coords[i][0]:.7f}  {coords[i][1]:.7f}  {coords[i][2]:.7f}\n")
 
-def main(csvDir , outputDir):
+
+def main(csvDir, outputDir):
     df = pd.read_csv(csvDir)
     colList = list(df.columns)
     colBox = boxGen(colList)
+
     while True:
-        smileIdx = input(f"Here are the columns provided in the dataframe: {csvDir} Enter the number for the column corresponding to the SMILES strings {colBox}")
+        smileIdx = input(
+            f"Here are the columns provided in the dataframe: {csvDir} "
+            f"Enter the number for the column corresponding to the SMILES strings {colBox}"
+        )
         try:
             smilesIdx = int(smileIdx)
             smilesCol = colList[smilesIdx]
             break
         except:
             print("Error: please enter an appropriate integer")
+
     while True:
-        idIdx = input(f"Here are the columns provided in the dataframe: {csvDir} Enter the number for the column corresponding to the ID strings {colBox}")
+        idIdx = input(
+            f"Here are the columns provided in the dataframe: {csvDir} "
+            f"Enter the number for the column corresponding to the ID strings {colBox}"
+        )
         try:
             idIdx = int(idIdx)
             idCol = colList[idIdx]
             break
         except:
             print("Error: please enter an appropriate integer")
+
     for _, row in df.iterrows():
         smiles = row[smilesCol]
         id = row[idCol]
-        elements , coords = smiles_to_coords(smiles)
-        xyzDir = outputDir + "/" + str(id)
-        xyzFile = xyzDir + "/" + str(id) + ".xyz"
-        if not os.path.exists(xyzDir):
-            os.makedirs(xyzDir)
+        elements, coords = smiles_to_coords(smiles)
+
+        xyzDir = Path(outputDir) / str(id)
+        xyzFile = xyzDir / f"{id}.xyz"
+
+        xyzDir.mkdir(parents=True, exist_ok=True)
         write_xyz(elements, coords, xyzFile)
 
 
 if __name__ == "__main__":
-    smilesCSV = str(sys.argv[1])
-    outputDir = str(sys.argv[2])
-    if not os.path.exists(outputDir):
-        os.makedirs(outputDir)
-    main(smilesCSV , outputDir)
+    smilesCSV = Path(sys.argv[1])
+    outputDir = Path(sys.argv[2])
+
+    outputDir.mkdir(parents=True, exist_ok=True)
+    main(smilesCSV, outputDir)
