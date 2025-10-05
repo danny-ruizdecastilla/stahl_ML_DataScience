5c144c5b964ed68b322e60ce9fe70eb5734f7c90
Stahl_PC <dannyrdc@sas.upenn.edu>
2025-10-04 20:50:40 -0500

    update

--- DFTWorkflow/dftFeatureExtractorMAST.py
+++ DFTWorkflow/dftFeatureExtractorMAST.py
@@ -8,24 +8,68 @@ import pandas as pd
 import numpy as np
 import chemdraw
 import base64
-from io import BytesIO
+from rdkit import Chem
 parentDir = Path(__file__).resolve().parents[1]
 sys.path.append(str(parentDir))
 from DFTWorkflow.cleanLogs import basicTerm
+from DFTWorkflow.ionComGenerator import locateinLog
+from DFTWorkflow.fukuiGenerator.fukuiExtractorV1 import  getBoltzmannWeightsGauss
 from dimensionalityReduction.reactivityFeatures import boxGen
+from breadthFirstSearch.radialBasedCorrelation import getCC
+from reaxysProcessing.reaxysSubstrateExtractorV2 import listInputs
+def extractShiftsByIdx(logFile: str, extract1:str, extract2:str,location1:str ,location2, idxList  , m , b):
+    lowerInd = locateinLog(logFile , extract1, location1)
+    upperInd = locateinLog(logFile , extract2, location2 )
+    if upperInd == "Poison" or lowerInd == "Poison":
+        raise ValueError(f"The Log file {logFile} did not terminate properly")
+    atomShifts = {}
+    with open(logFile , "r") as f:
+        for idx, line in enumerate(f):
+            if idx > int(lowerInd)  and idx < int(upperInd):
+                
+                if "   Isotropic =  " in line:
+                    atomInd = line.split("   Isotropic =  ")[0].strip()
+                    atomIdx = int(atomInd.split(" ")[0].strip())
+                    if atomIdx in idxList:
+                        shiftsTots = line.split("   Isotropic =  ")[-1].strip()
+                        shifts = shiftsTots.split("Anisotropy")[0]
+                        atomName = str(atomInd.split(" ")[-1].strip())
+                        atomShifts[atomIdx] = [ atomName,(b - float(shifts.strip())) / m]
+    return atomShifts  
+def getAlkenes(substratesHash , smilesHash , featureList):
 
-def C13Assign(molecHash , subMolec): #for subMolecules that experience symmetry C13 shifts are used to assign C1 and C2 
-    #molecHash = {id : {smiles: smilesStr , subMolecId : {nmrHash} } , }
-    config = chemdraw.Config()
-    config.atom_numbers.show = True
-    drawer = chemdraw.Drawer(subMolec, title=subMolec, config= config)
-    buf = BytesIO()
-    fig = drawer.draw()
-    fig.write_image(buf, format="png")  # needs kaleido installed
-    buf.seek(0)
-    subMolec64 = base64.b64encode(buf.read()).decode("utf-8")
-def configureSubstructure(smilesMAST , idMAST , filteredLogs , symmetryCheck):
-
+    for id , smilesStr in smilesHash.items():
+        substrateHash = {}
+        cc , molec = getCC(smilesStr)
+        conformerFiles = substratesHash[id]
+        boltzmannDF = getBoltzmannWeightsGauss(conformerFiles, 298, "electronic")
+        if "C13_shift" in featureList:
+            C13_C1 = []
+            C13_C2 = []
+            
+            for name in list(boltzmannDF["logID"]):
+                fileStr = f"{name}.log"
+                conformer  = next((f for f in conformerFiles if fileStr in f.name), None)
+                alkeneHash = extractShiftsByIdx(conformer, "SCF GIAO Magnetic shielding tensor (ppm):", "Eigenvalues:  ", "earliest", "latest", cc , 1.0081 , 195.6683)
+                if len(C13_C1) == 0:
+                    #C1 and C2 not yet defined 
+                    atom0 = alkeneHash[cc[0]][1]
+                    atom1 = alkeneHash[cc[1]][1]
+                    if atom0 >= atom1:#C1 is smallest shift, C2 is largest shift
+                        C1 = cc[1]
+                        C2 = cc[0]
+                    else:
+                        C1 = cc[0]
+                        C2 = cc[1]
+                C13_C1.append(alkeneHash[C1][1])
+                C13_C2.append(alkeneHash[C2][1])
+            weights = boltzmannDF["boltzWeights"]
+            C13_Cmx = ( (C13_C2 * weights).sum()    / weights.sum()  )
+            C13_Cmn = ( (C13_C1 * weights).sum()    / weights.sum()  )
+            C13_delta = C13_Cmx - C13_Cmn
+            C13_Mean = np.mean([C13_Cmx,C13_Cmn])
+        for feature in featureList.drop("C13_shift"):
+            if feature == ""
 
 def compartmentalization(logDir , outputDir , substrateFile):
     substrateDF = pd.read_csv(substrateFile)
@@ -56,15 +100,36 @@ def compartmentalization(logDir , outputDir , substrateFile):
     logFiles = logPaths.glob('*.log')
     fileSplit = input(f"{logFiles[0]} Enter the string iteral that seperates the common name with the conf. type : ")
 
-    filteredLogs = []
+    substrateHash = {}
+    smilesHash = {}
     for log in logFiles:
         termError = basicTerm(log , "Error termination" , "Normal termination")
         if not termError:
-            filteredLogs.append(log)
+            substrates = list(substrateHash.keys())
+            fileID = log.name.split(fileSplit)[0]
+            if fileID in substrates:
+                substrateHash[fileID].append(log)
+            else:
+                substrateHash[fileID] = [log]
+                smilesHash[fileID] = smilesMAST[idMAST.index(fileID)]
+    extractNum = input(f"Enter the number corresponding to which substructre you want to extract information from:\n [0] Alkenes\n")
+    if int(extractNum) == 0:
+        localStrs = ["C13_shift" , "NBO7" , "fukuiParameters" , "%Vbur" , "EvsZ" , "Dist." ]
+        localDescriptorsInput = boxGen(localStrs)
+        featureList = listInputs(f"Enter the indexes corresponding to the features you would like to extract\n{localDescriptorsInput}")
+        featuresMAST = []
+        for idx in featureList:
+            feature = featureList[idx]
+            featuresMAST.append(feature)
+        substratesMAST = getAlkenes(substrateHash , smilesHash , featuresMAST)
 
-    subMolecHash , substrateIDHash = configureSubstructure(smilesMAST , idMAST , filteredLogs , True)
+
+
+
+    
 
 if __name__ == "__main__":
     logDir = str(sys.argv[1])
     outputDir = str(sys.argv[2])
     substrateCSV = str(sys.argv[3])
+    compartmentalization(logDir , outputDir, substrateCSV)
