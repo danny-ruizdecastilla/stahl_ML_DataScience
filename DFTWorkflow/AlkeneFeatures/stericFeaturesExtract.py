797854d9c15594c03b0df3292d927d20950aa813
danny-ruizdecastilla <dannyrdc@sas.upenn.edu>
2026-01-07 21:49:00 -0600

    update

--- DFTWorkflow/AlkeneFeatures/stericFeaturesExtract.py
+++ DFTWorkflow/AlkeneFeatures/stericFeaturesExtract.py
@@ -13,6 +13,7 @@ from itertools import islice
 parentDir = Path(__file__).resolve().parents[2]
 sys.path.append(str(parentDir))
 from DFTWorkflow.ionComGenerator import locateinLog
+from DFTWorkflow.AlkeneFeatures.alkeneStericDougnut import alkeneSemiCylinders
 from dimensionalityReduction.reactivityFeatures import boxGen
 from breadthFirstSearch.radialBasedCorrelation import getCC
 from reaxysProcessing.reaxysSubstrateExtractorV2 import listInputs
@@ -72,16 +73,56 @@ class CrestChemical:
             boltzmannFacts =[np.exp((-1 * e) / (R * temperature)) for e in rE_kCal ]
             self.boltzmannFactors = boltzmannFacts 
             self.relEnergies = rE_kCal
-    def getBurriedVol(self , atoms , radList ):
-
+    def getBurriedVol(self , atoms , radList ): #atoms = { idx : ["C"  , "alkene"] , ix : ["C" , "alkene2"]}
+        burriedVolumeHash = {}
+        boltzSum = sum(self.boltzmannFactors)
         for rad in radList:
+            vBurrList = []
             for atom , name in atoms.items():
                 symbol = name[0]
                 nameStr = name[1]
                 saveStr = f"{rad}_Ang_Vburr_{symbol}{nameStr}"
                 enum = 0
+                totVburr = 0
                 for energy , coordsHash in self.conformersHash.items():
                     boltzFact = self.boltzmannFactors[enum]
-                    vbur = BuriedVolume(coordsHash["atoms"],coordsHash["coordinates"],atom,include_hs=True,radius=rad).fraction_buried_volume
-                    
+                    vBur = BuriedVolume(coordsHash["atoms"],coordsHash["coordinates"],atom,include_hs=True,radius=rad).fraction_buried_volume
                     enum +=1
+                    weightedVbur = boltzFact * vBur
+                    tutVburr += weightedVbur
+                avgVbur = totVburr / boltzSum
+                burriedVolumeHash[saveStr] = avgVbur
+                vBurrList.append(avgVbur)
+            meanVburr = np.mean(vBurrList)
+            deltaVburr = abs(vBurrList[0] - vBurrList[1])
+            burriedVolumeHash[f"{rad}_Ang_Vburr_mean"] = meanVburr
+            burriedVolumeHash[f"{rad}_Ang_Vburr_delta"] = deltaVburr
+        return burriedVolumeHash
+    def getBurriedCylinder(self , alkeneIndex , radList , g ):
+        burriedVolumeHash = {}
+        boltzSum = sum(self.boltzmannFactors)
+        for rad in radList:
+            enum = 0
+            for energy , coordsHash in self.conformersHash.items():
+                boltzFact = self.boltzmannFactors[enum]
+                coordHash = {}
+                for i in range(self.NumAtoms):
+                    atom = coordsHash["atoms"][i]
+                    coordinates = coordsHash["coordinates"][i]
+                    coordHash[i] = [atom , "0" ]
+                    for coord in coordinates:
+                        coordHash[i].append(coord)
+                    if i in alkeneIndex:
+                        contacts  =  list(g.neighbors(i))
+                        
+                mainCylinder = alkeneSemiCylinders(C1Hash , C2Hash , rad)
+                mainCylinder.getAtoms(coordHash)
+                maxSemi , minSemi = mainCylinder.getBurriedVolume(True)
+                Cburr = mainCylinder.getBurriedVolume(False)
+                enum +=1
+                weightedVbur = boltzFact * vBur
+                tutVburr += weightedVbur
+            avgVbur = totVburr / boltzSum
+            burriedVolumeHash[saveStr] = avgVbur
+
+        return burriedVolumeHash
