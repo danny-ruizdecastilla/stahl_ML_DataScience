375ca48461a1620ebdb91abf302bb8bec69be8e5
danny-ruizdecastilla <dannyrdc@sas.upenn.edu>
2026-01-07 18:28:48 -0600

    update

--- DFTWorkflow/AlkeneFeatures/stericFeaturesExtract.py
+++ DFTWorkflow/AlkeneFeatures/stericFeaturesExtract.py
@@ -19,8 +19,9 @@ from reaxysProcessing.reaxysSubstrateExtractorV2 import listInputs
 #Extract Steric Features from Crest raw XTB coordinates 
 
 class CrestChemical:
-    def __init__(self, name, strucFile, energyFile):
+    def __init__(self, name, smiles , strucFile, energyFile):
         self.name = name
+        self.smiles = smiles
         self.strucFile = strucFile
         with open(strucFile, 'r') as f:
             counts = 0
@@ -44,7 +45,7 @@ class CrestChemical:
             collectingCoordinates = False
             for idx , line in enumerate(f):
                 if not collectingCoordinates and line.strip() == self.NumAtoms:   #new conformer!
-                    coordHash = {}
+                    coordHash = {"atoms" : [] , "coordinates" : []}
                     collectingCoordinates = True
                     atomCount = 0
                 elif collectingCoordinates and is_float(line.strip()):
@@ -52,19 +53,35 @@ class CrestChemical:
                 elif collectingCoordinates and line.split("         ")[0].strip()[:1].isalpha():
                     lineOptions = line.strip().split("    ")
                     atom = str(lineOptions[0])
-                    coords = [num.strip() for num in lineOptions if is_float(num.strip())]
-                    coordHash[str(idx) + "," + atom] = coords
+                    coords = [float(num.strip()) for num in lineOptions if is_float(num.strip())]
+                    coordHash["atoms"].append(atom)
+                    coordHash["coordinates"].append(np.array(coords))
                     atomCount +=1
                     if atomCount == int(self.NumAtoms):
                         self.conformersHash[energyLevel] = coordHash 
                         collectingCoordinates = False
         return self.conformersHash
+    def getBoltzmannWeights(self , hartree , temperature ):
+        if hartree:
+            R = 1.987204e-3
+            HARTREE_TO_KCAL = 627.5094740631
+            energies = list(self.conformersHash.keys())
+            kCalE = [e * HARTREE_TO_KCAL for e in energies ]
+            minE = min(kCalE)
+            rE_kCal = [e - minE for e in kCalE ]
+            boltzmannFacts =[np.exp((-1 * e) / (R * temperature)) for e in rE_kCal ]
+            self.boltzmannFactors = boltzmannFacts 
+            self.relEnergies = rE_kCal
+    def getBurriedVol(self , atoms , radList ):
 
-
-class Conformer:
-    def __init__(self, parent, xyz, energy):
-        if parent is None:
-            raise ValueError("Conformer must have a parent CrestChemical")
-        self.parent = parent
-        self.xyz = xyz
-        self.energy = energy
+        for rad in radList:
+            for atom , name in atoms.items():
+                symbol = name[0]
+                nameStr = name[1]
+                saveStr = f"{rad}_Ang_Vburr_{symbol}{nameStr}"
+                enum = 0
+                for energy , coordsHash in self.conformersHash.items():
+                    boltzFact = self.boltzmannFactors[enum]
+                    vbur = BuriedVolume(coordsHash["atoms"],coordsHash["coordinates"],atom,include_hs=True,radius=rad).fraction_buried_volume
+                    
+                    enum +=1
