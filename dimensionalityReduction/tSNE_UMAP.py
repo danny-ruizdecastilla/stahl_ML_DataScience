97e66d8ebc3cb71e0830d9b291a1ac231812ff9a
Stahl_PC <dannyrdc@sas.upenn.edu>
2026-02-13 18:01:31 -0600

    update

--- dimensionalityReduction/tSNE_UMAP.py
+++ dimensionalityReduction/tSNE_UMAP.py
@@ -13,7 +13,7 @@ sys.path.append(parentDir)
 from reaxysProcessing.reaxysSubstrateExtractorV2 import listInputs
 from dimensionalityReduction.reactivityFeatures import boxGen
 
-def generate_UMAP_tSNE(X):
+def generate_UMAP_tSNE(X, makeUMap:False):
     colList = list(X.columns)
     dfColBox = boxGen(colList)
     elimCols = listInputs(f"Here are all available columns for the dataframe {dfColBox}\n please enter the matching indexes corresponding to the columns you want to remove from the dataframe:\n")
@@ -39,40 +39,39 @@ def generate_UMAP_tSNE(X):
     tsne = TSNE(n_components=2, perplexity=int(perplexSilScoreMax), random_state=0)
     tSNE_1 = X_tsne[:, 0] 
     tSNE_2 = X_tsne[:, 1]
-    UMAP_Hash ={}
-    for n_neighbors in [30,50,70,90,110,130,150]:
-        for min_dist in [0.0, 0.1, 0.25 , 0.5]:
-            reducer = umap.UMAP(n_neighbors=n_neighbors, min_dist=min_dist, random_state=42)
-            X_umap = reducer.fit_transform(X_scaled)
-            
-            kmeans = KMeans(n_clusters=15, random_state=42)
-            labels = kmeans.fit_predict(X_umap)
-            
-            sil = silhouette_score(X_umap, labels)
-            UMAP_Hash[sil] = [n_neighbors , min_dist]
-    silScoreUMAP = max(list(UMAP_Hash.keys()))  
-    values = UMAP_Hash[silScoreUMAP]
-    reducer = umap.UMAP(n_neighbors=values[0], min_dist=values[1], random_state=42)
-    X_umap = reducer.fit_transform(X_scaled)
-    UMAP_1 = X_umap[:, 0]
-    UMAP_2 = X_umap[:, 1]
-    return UMAP_1, UMAP_2,tSNE_1,tSNE_2
+    if makeUMap:
+        UMAP_Hash ={}
+        for n_neighbors in [30,50,70,90,110,130,150]:
+            for min_dist in [0.0, 0.1, 0.25 , 0.5]:
+                reducer = umap.UMAP(n_neighbors=n_neighbors, min_dist=min_dist, random_state=42)
+                X_umap = reducer.fit_transform(X_scaled)
+                
+                kmeans = KMeans(n_clusters=15, random_state=42)
+                labels = kmeans.fit_predict(X_umap)
+                
+                sil = silhouette_score(X_umap, labels)
+                UMAP_Hash[sil] = [n_neighbors , min_dist]
+        silScoreUMAP = max(list(UMAP_Hash.keys()))  
+        values = UMAP_Hash[silScoreUMAP]
+        reducer = umap.UMAP(n_neighbors=values[0], min_dist=values[1], random_state=42)
+        X_umap = reducer.fit_transform(X_scaled)
+        UMAP_1 = X_umap[:, 0]
+        UMAP_2 = X_umap[:, 1]
+        return UMAP_1, UMAP_2,tSNE_1,tSNE_2
+    else:
+        return 0 , 0 , tSNE_1,tSNE_2
 
 def main(df:np.array , outputDir:str , featureType:str):
     from pathlib import Path
     from dimensionalityReduction.kMeansClustering import kMeansCluster
-    UMAP_1, UMAP_2,tSNE_1,tSNE_2 = generate_UMAP_tSNE(df)
-    df[f"{featureType}_UMAP_1"] = UMAP_1
-    df[f"{featureType}_UMAP_2"] = UMAP_2
+    UMAP_1, UMAP_2,tSNE_1,tSNE_2 = generate_UMAP_tSNE(df ,False)
+    #df[f"{featureType}_UMAP_1"] = UMAP_1
+    #df[f"{featureType}_UMAP_2"] = UMAP_2
     df[f"{featureType}_tSNE_1"] = tSNE_1
     df[f"{featureType}_tSNE_2"] = tSNE_2
     outputDir_tSNE = Path(outputDir) / "tSNE"
     outputDir_tSNE.mkdir(parents=True, exist_ok=True)
-    dftSNE = kMeansCluster(df, f"{featureType}_tSNE_1", f"{featureType}_tSNE_1", 15, 45, "tSNE" , outputDir_tSNE)
-    dftSNE.to_csv(outputDir_tSNE  / f"clustered_{featureType}.csv")
-    outputDir_UMAP = Path(outputDir) / "UMAP"
-    outputDir_UMAP.mkdir(parents=True, exist_ok=True)
-    dftSNE = kMeansCluster(df, f"{featureType}_UMAP_1", f"{featureType}_UMAP_1", 15, 45 , "UMAP" , outputDir_tSNE)
+    dftSNE = kMeansCluster(df, f"{featureType}_tSNE_1", f"{featureType}_tSNE_2", 15, 45, "tSNE" , outputDir_tSNE)
     dftSNE.to_csv(outputDir_tSNE  / f"clustered_{featureType}.csv")
 if __name__ == "__main__":
     datasetDir = str(sys.argv[1])
