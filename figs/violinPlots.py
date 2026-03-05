afc7bd2963a24c8bd3bdf5004a4b0077dca54c51
danny-ruizdecastilla <dannyrdc@sas.upenn.edu>
2026-03-05 11:37:25 -0600

    update

--- figs/violinPlots.py
+++ figs/violinPlots.py
@@ -2,7 +2,9 @@
 #Daniel Ruiz de Castilla | 03.04.2026
 import plotly.express as px
 import plotly.graph_objects as go
+from dash import html
 from pathlib import Path
+import pandas as pd
 import sys
 parentDir = Path(__file__).resolve().parents[1]
 sys.path.append(str(parentDir))
@@ -23,32 +25,47 @@ def violinTemplate():
     template.layout.yaxis.showgrid = False
     template.layout.yaxis.mirror = True
     return template
+def askingForAddition(askStr):
+    response = input(f"{askStr}").strip().lower()
 
-def make_violin_plots(df, columns, box=True, meanline=True):
-    """
-    Create violin plots from selected DataFrame columns.
-    
-    Parameters:
-        df (pd.DataFrame): source dataframe
-        columns (list): list of column names to plot
-        box (bool): show mini boxplot inside violin
-        meanline (bool): show mean line
-    """
-    
-    fig = go.Figure()
-    
-    for col in columns:
-        if col not in df.columns:
-            raise ValueError(f"Column '{col}' not found in DataFrame")
-        
+    if response in {"yes", "y"}:
+        return True
+    elif response in {"no", "n"}:
+        return False
+    else:
+        print("Please enter a valid 'yes' or 'no' response.")
+        return askingForAddition(askStr)   # recursive call
+def checkCol(df):
+    colStr = input("Enter the name of the column from the dataframe you want to add: ").strip()
+
+    if colStr in df.columns:
+        return colStr
+    else:
+        print(f"'{colStr}' is not a valid column.")
+        print("Available columns:")
+        print(", ".join(df.columns))
+        return checkCol(df)
+
+def compareSources(fig , saveDir):
+    addSources = askingForAddition("Do you want to add another source to your violin plot? (yes/no): ")
+
+    if addSources:
+        dfDir = input("Enter the source of the dataframe: ").strip()
+        df = pd.read_csv(dfDir)
+        col = checkCol(df)
         fig.add_trace(go.Violin(
             y=df[col].dropna(),
             name=col,
-            box_visible=box,
-            meanline_visible=meanline
+            box_visible=True,
+            meanline_visible=True
         ))
-    
-    fig.update_layout(template=violinTemplate())
-    fig.show()
-    
-    return fig
+        
+        fig.update_layout(template=violinTemplate())
+        
+        return compareSources(fig)
+    else:
+        fig.write_html(saveDir) 
+if __name__ == "__main__":
+    saveDir = str(sys.argv[1])
+    fig = go.Figure()
+    compareSources(fig , saveDir )
