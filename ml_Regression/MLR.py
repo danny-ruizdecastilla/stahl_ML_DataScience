3bb921ff22004ffd955f1a3c834df592e37aeb15
Stahl_PC <dannyrdc@sas.upenn.edu>
2026-06-16 10:51:37 -0500

    update

--- ml_Regression/MLR.py
+++ ml_Regression/MLR.py
@@ -42,19 +42,22 @@ def MLR(df , yCol , paramNum):
         X = scaler.fit_transform(X)
         rmseDump = []
         maeDump = []
+        rmseTrain = []
         for i in range(5):
             mlr = LinearRegression()
             X_train, X_test, y_train, y_test = train_test_split(X, yCol, test_size=0.3, random_state=i)
             mlr.fit(X_train,y_train)
             y_fit = mlr.predict(X_test)
+            y_fitT = mlr.predict(X_train)
+            rmseTrain.append(rmseCalc(y_fitT , y_train))
             rmse = rmseCalc(y_fit , y_test)
             mae = maeCalc(y_fit , y_test)
             rmseDump.append(rmse)
             maeDump.append(mae)
-        dataHash = {"rmseDump" : rmseDump , "maeDump" : maeDump , "rmseAvg" : np.median(rmseDump)}
+        dataHash = {"rmseDump" : rmseDump , "maeDump" : maeDump , "rmseAvg" : np.mean(rmseDump) , "rmseTrainAvg" : np.mean(rmseTrain)}
         resultStr = ""
         for col in cols:
-            resultStr += f"{col}_"
+            resultStr += f"{col}&"
         resultsHash[resultStr] = dataHash
     
     sortedResults = dict(
@@ -73,12 +76,12 @@ if __name__ == "__main__":
     outPath = Path(outStr)
     outPath.mkdir(parents=True, exist_ok=True)
     dfMain = pd.read_csv(dfStr)
-    yCol = dfMain["RelRate"]
+    #yCol = dfMain["RelRate"]
     yCol2 = dfMain["k2"]
     yCol3 = dfMain["err"]
-    dfMain = dfMain.drop(columns=["RelRate" , "k2" , "err" , "SMILES" , "Canonicals" , "ID"])
+    dfMain = dfMain.drop(columns=[ "k2" , "err" , "SMILES" , "Canonicals" , "ID"])
     MLRHash = MLR(dfMain , yCol2 , numParms)
     MLRFile = outPath / f"{outName}_MLR.dat"
     with MLRFile.open("w") as f:
         for key , val in MLRHash.items():
-            f.write(f"{key} {val}\n")
+            f.write(f"{key} : {val}\n\n")
